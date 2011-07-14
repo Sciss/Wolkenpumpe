72310a5a4ddf8bc8ef2c1cc3f4b420b2f2b5d1ae
Sciss <contact@sciss.de>
2011-07-14 15:51:43 +0100

    ...

--- project/build/Wolkenpumpe.scala
+++ project/build/Wolkenpumpe.scala
@@ -4,12 +4,29 @@ import sbt.{ FileUtilities => FU, _}
 class WolkenpumpeProject( info: ProjectInfo ) extends DefaultProject( info ) {
    val scalaColliderSwing  = "de.sciss" %% "scalacolliderswing" % "0.27"
    val soundProcesses      = "de.sciss" %% "soundprocesses" % "0.23"
-//   // for some reason, we need to add the snapshot repos here again...
-//   val ccstmRepo           = "CCSTM Release Repository at PPL" at "http://ppl.stanford.edu/ccstm/repo-releases"
-//   val ccstmSnap           = "CCSTM Snapshot Repository at PPL" at "http://ppl.stanford.edu/ccstm/repo-snapshots"
-//   val prefuse             = "prefuse" % "prefuse" % "beta-SNAPSHOT" from "http://github.com/downloads/Sciss/ScalaColliderSwing/prefuse-beta-SNAPSHOT.jar"
-   val prefuse             = "de.sciss" % "prefuse-core" % "0.20"
 
    // for some reason, we need to add the jsyntaxpane repo here again...
    val repo1               = "Clojars Repository" at "http://clojars.org/repo"
+
+   // ---- publishing ----
+
+   override def managedStyle  = ManagedStyle.Maven
+   val publishTo              = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
+
+//   override def packageDocsJar= defaultJarPath( "-javadoc.jar" )
+//   override def packageSrcJar = defaultJarPath( "-sources.jar" )
+//   val sourceArtifact         = Artifact.sources( artifactID )
+//   val docsArtifact           = Artifact.javadoc( artifactID )
+//   override def packageToPublishActions = super.packageToPublishActions ++ Seq( packageDocs, packageSrc )
+
+   override def pomExtra =
+      <licenses>
+        <license>
+          <name>GPL v2+</name>
+          <url>http://www.gnu.org/licenses/gpl-2.0.txt</url>
+          <distribution>repo</distribution>
+        </license>
+      </licenses>
+
+   Credentials( Path.userHome / ".ivy2" / ".credentials", log )
 }
