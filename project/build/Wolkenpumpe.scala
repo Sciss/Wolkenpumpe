9a8754d39c9c3cf3bfb988bb78e6b79e110d068f
Sciss <contact@sciss.de>
2011-07-14 15:03:39 +0100

    scala 2.9.0-1

--- project/build/Wolkenpumpe.scala
+++ project/build/Wolkenpumpe.scala
@@ -1,16 +1,14 @@
 import xml._
 import sbt.{ FileUtilities => FU, _}
 
-/**
- *    @version 0.12, 02-Aug-10
- */
 class WolkenpumpeProject( info: ProjectInfo ) extends DefaultProject( info ) {
    val scalaColliderSwing  = "de.sciss" %% "scalacolliderswing" % "0.26"
    val soundProcesses      = "de.sciss" %% "soundprocesses" % "0.23"
 //   // for some reason, we need to add the snapshot repos here again...
 //   val ccstmRepo           = "CCSTM Release Repository at PPL" at "http://ppl.stanford.edu/ccstm/repo-releases"
 //   val ccstmSnap           = "CCSTM Snapshot Repository at PPL" at "http://ppl.stanford.edu/ccstm/repo-snapshots"
-   val prefuse             = "prefuse" % "prefuse" % "beta-SNAPSHOT" from "http://github.com/downloads/Sciss/ScalaColliderSwing/prefuse-beta-SNAPSHOT.jar"
+//   val prefuse             = "prefuse" % "prefuse" % "beta-SNAPSHOT" from "http://github.com/downloads/Sciss/ScalaColliderSwing/prefuse-beta-SNAPSHOT.jar"
+   val prefuse             = "de.sciss" % "prefuse" % "0.20"
 
    // for some reason, we need to add the jsyntaxpane repo here again...
    val repo1               = "Clojars Repository" at "http://clojars.org/repo"
